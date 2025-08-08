c0237f521a23ca1479f246d3b4933ff0a94c3b83
camslaw <ccestridge8@gmail.com>
2025-08-08 19:20:42 -0400

    Make qml file a resource and adjust mcos preset

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-    QQmlApplicationEngine engine("../qml/main.qml");
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
+    if (engine.rootObjects().isEmpty()) return -1;
+
     return app.exec();
 }
